c6a77dd3dbebd57ea470029fb35fa4506c64bb90
LESAGEjf <j.flesage@hotmail.fr>
2020-03-24 12:50:51 +0100

    helloooo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { MonPremierComponent } from './mon-premier/mon-premier.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonPremierComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
