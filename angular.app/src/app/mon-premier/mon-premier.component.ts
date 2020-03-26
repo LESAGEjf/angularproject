4b5657d5d6a9102e3ddd33947997f7f92badbd8c
LESAGEjf <j.flesage@hotmail.fr>
2020-03-26 15:47:44 +0100

    html modified

--- angular.app/src/app/mon-premier/mon-premier.component.ts
+++ angular.app/src/app/mon-premier/mon-premier.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MonPremierComponent implements OnInit {
 
+  public username = "toto";
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
